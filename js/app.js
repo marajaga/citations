b0b2ef21824800839635c0395a85ff3dce32bb78
Fanny <fanny.pringere@icloud.com>
2021-02-22 10:48:17 +0100

    fixed css html js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
-let facts = ["” Accepte ce qui est, laisse aller ce qui était, aie confiance en ce qui sera.” - Bouddha",
-    "L’obscurité ne peut pas chasser l’obscurité, seule la lumière le peut. La haine ne peut pas chasser la haine, seul l’amour le peut.” - Martin Luther King",
+let facts = ["Accepte ce qui est, laisse aller ce qui était, aie confiance en ce qui sera.” - Bouddha",
     "On a deux vies. La deuxième commence quand on réalise qu’on n’en a qu’une.” - Confucius",
     "La vie, ce n’est pas d’attendre que les orages passent, c’est d’apprendre à danser sous la pluie.” - Sénèque",
     "S’il y a un problème, il y a une solution. S’il n’y a pas de solution, alors ce n’est pas un problème.",
-    "Si vous pouvez le rêver, vous pouvez le faire.” - Walt Disney",
     "J’ai décidé d’être heureux parce que c’est bon pour la santé.” - Voltaire",
+    "Ne perds jamais espoir, lorsque le soleil se couche, les étoiles apparaissent” - Walter Bagehot",
     "Ne t’inquiète pas de l’échec. Inquiète-toi de ce que tu manques si tu n’essayes même pas.” - Jack Canfield",
     "Croyez en vos rêves et ils se réaliseront peut-être. Croyez en vous et ils se réaliseront sûrement.” - Martin Luther King",
+    "N’acceptez jamais la défaite, vous êtes peut-être à un pas de la réussite.” - Denis Waitley",
     "La vie c’est comme une bicyclette, il faut avancer pour ne pas perdre l’équilibre.” - Albert Einstein"];
 
 let colors = ["LightSalmon", "LightPink", "LightSeaGreen"];
